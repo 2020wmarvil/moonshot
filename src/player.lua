71fa6068db26effb0fce1e8e8305a354cd259a43
Wyatt Marvil <2020wmarvil@gmail.com>
2020-11-28 14:10:29 -0500

    Add a player sprite

--- src/player.lua
+++ src/player.lua
@@ -1,9 +1,9 @@
 Player = Object:extend()
 
-function Player:new(x, y, image)
+function Player:new(x, y)
   self.x = x
   self.y = y
-  self.image = love.graphics.newImage(image)
+  self.image = love.graphics.newImage("assets/player.png")
   self.width = self.image:getWidth()
   self.height = self.image:getHeight()
   self.speedX = 100
@@ -14,5 +14,5 @@ function Player:update(dt)
 end
 
 function Player:draw()  
-  love.graphics.draw(self.image, self.x, self.y, 0, 1, 1, self.width / 2, self.height / 2)
+  love.graphics.draw(self.image, self.x, self.y, 0, 4, 4, math.floor(self.width / 2), math.floor(self.height / 2))
 end
